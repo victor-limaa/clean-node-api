98115808226b7e14d98f98258b0e74faa5de7da1
victor-limaa <victor-limaa@hotmail.com>
2024-03-18 23:22:47 -0300

    feat: signup return error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,13 @@
 interface httpResponse {
   statusCode: number
+  body: any
 }
 
 export class SignUpController {
   handle(httpRequest: any): httpResponse {
     return {
       statusCode: 400,
+      body: new Error('Missing param: name'),
     }
   }
 }
