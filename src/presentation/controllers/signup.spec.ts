abdc55d361d493f7b67b1989794801f1eec52d57
victor-limaa <victor-limaa@hotmail.com>
2024-03-20 08:22:46 -0300

    refactor: create function makeSut

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamsError } from '../errors/missing-params-errors'
 import { SignUpController } from './signup'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('signup', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any email',
@@ -17,7 +21,7 @@ describe('signup', () => {
   })
 
   test('should return 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any name',
@@ -31,7 +35,7 @@ describe('signup', () => {
   })
 
   test('should return 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any name',
@@ -45,7 +49,7 @@ describe('signup', () => {
   })
 
   test('should return 400 if no passwordConfirmation is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'any name',
