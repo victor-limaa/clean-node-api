928510dc23461288c7f154f0ff02d2fd821fbd88
victor-limaa <victor-limaa@hotmail.com>
2024-03-21 22:44:44 -0300

    refactor: refact server error to http-helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamsError } from '../errors/missing-params-errors'
-import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
@@ -31,10 +30,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError(),
-      }
+      return serverError()
     }
   }
 }
